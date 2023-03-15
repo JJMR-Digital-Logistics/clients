98de3ce14fc2da488d48eec85841e7731887653b
Marco <pastry07@gmail.com>
2023-03-14 19:56:00 -0700

    signin and signup functional

--- inquirer.js
+++ inquirer.js
@@ -1,6 +1,12 @@
 'use strict';
 
+const axios = require('axios');
 const inquirer = require('inquirer');
+const base64 = require('base-64');
+require('dotenv').config();
+
+let localUser;
+let axiosUser;
 
 inquirer
   .prompt([
@@ -13,18 +19,20 @@ inquirer
   ])
   .then((answer) => {
     if (answer.user_choice === 'Signin') {
+      clearLocalUser();
       signIn();
-      console.log('Welcome to your signin');
+      //console.log('Welcome to your signin');
     }
     else if (answer.user_choice === 'Signup') {
+      clearLocalUser();
       signUp();
-      console.log('Please enter your info to signup');
+      //console.log('Please enter your info to signup');
     }
 
   });
 
 const signIn = () => {
-  let user;
+
   inquirer
     .prompt([
       {
@@ -33,20 +41,42 @@ const signIn = () => {
         message: 'Please enter your username',
       },
       {
-        name: 'pasword',
+        name: 'pWord',
         type: 'password',
         message: 'Please enter your password',
       },
     ])
     .then((answer) => {
-      user.username = answer.userName;
-      user.password = answer.password;
-      console.log(user);
+      localUser.username = answer.userName;
+      localUser.password = answer.pWord;
+      console.log(localUser);
+      handleSignin(localUser);
+
+
     });
 };
 
+const handleSignin = async (user) => {
+  let url = `${process.env.EXPRESS_SERVER}/signin`;
+  let userStr = `${user.username}:${user.password}`;
+  let encodedUser = base64.encode(userStr);
+  let config = {
+    url,
+    method: 'post',
+    headers: {
+      'authorization': `basic ${encodedUser}`,
+    },
+
+  };
+  axiosUser = await axios(config);
+  console.log(axiosUser.data);
+
+};
+// headers: {
+//   "Authorization": `bearer ${token}`,
+// },
 const signUp = () => {
-  let user;
+  
   inquirer
     .prompt([
       {
@@ -55,7 +85,7 @@ const signUp = () => {
         message: 'Please enter your username',
       },
       {
-        name: 'pasword',
+        name: 'pWord',
         type: 'password',
         message: 'Please enter your password',
       },
@@ -67,12 +97,33 @@ const signUp = () => {
       },
     ])
     .then((answer) => {
-      user.username = answer.userName;
-      user.password = answer.password;
-      user.role = answer.role;
-      console.log(user);
+      localUser.username = answer.userName;
+      localUser.password = answer.pWord;
+      localUser.role = answer.role;
+      console.log(localUser);
+
+      handleSignUp(localUser);
     });
 };
+
+const handleSignUp = async (user) => {
+  let url = `${process.env.EXPRESS_SERVER}/signup`;
+  // let userStr = `${user.username}:${user.password}`;
+  // let encodedUser = base64.encode(userStr);
+  let config = {
+    url,
+    method: 'post',
+    data: {
+      'username': user.username,
+      'password': user.password,
+      'role': user.role,
+    },
+  };
+  axiosUser = await axios(config);
+  console.log(axiosUser.data);
+
+};
+
 
 const userMenu = () => {
   inquirer
@@ -104,11 +155,19 @@ const adminMenu = () => {
     });
 };
 
-const search = () => {};
-const Create = () => {};
-const Read = () => {};
-const Update = () => {};
-const Delete = () => {};
+const clearLocalUser = () => {
+  localUser = {
+    username: '',
+    password: '',
+    role: '',
+  };
+};
+
+const search = () => { };
+const Create = () => { };
+const Read = () => { };
+const Update = () => { };
+const Delete = () => { };
 
 
 
