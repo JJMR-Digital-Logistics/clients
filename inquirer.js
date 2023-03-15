9b5bb1985676678cef4f57f26523d2334eafec4e
Marco <pastry07@gmail.com>
2023-03-15 12:18:58 -0700

    inquirer CRUD functions complete

--- inquirer.js
+++ inquirer.js
@@ -6,7 +6,7 @@ const base64 = require('base-64');
 require('dotenv').config();
 
 let localUser;
-let axiosUser;
+let currentUser;
 
 inquirer
   .prompt([
@@ -28,11 +28,9 @@ inquirer
       signUp();
       //console.log('Please enter your info to signup');
     }
-
   });
 
 const signIn = () => {
-
   inquirer
     .prompt([
       {
@@ -46,22 +44,19 @@ const signIn = () => {
         message: 'Please enter your password',
       },
     ])
-    .then((answer) => {
+    .then(async (answer) => {
       localUser.username = answer.userName;
       localUser.password = answer.pWord;
       console.log(localUser);
-      handleSignin(localUser);
 
-      console.log(axiosUser);
-      // if(axiosUser.role === 'admin')
-      // {
-      //   adminMenu();
-      // }
-      // else 
-      // {
-      //   userMenu();
-      // }
-
+      currentUser = await handleSignin(localUser);
+      console.log('currentUser: ', currentUser);
+      if (currentUser.role === 'admin') {
+        adminMenu();
+      }
+      else {
+        userMenu();
+      }
     });
 };
 
@@ -73,13 +68,19 @@ const handleSignin = async (user) => {
     let config = {
       url,
       method: 'post',
-      headers: {
-        'authorization': `basic ${encodedUser}`,
+      auth: {
+        username: user.username,
+        password: user.password,
       },
+      // headers: {
+      //   'authorization': `basic ${encodedUser}`,
+      // },
 
     };
-    axiosUser = await axios(config);
-    //console.log(axiosUser);
+    let axiosUser = await axios(config);
+    //console.log('axiosUser', axiosUser);
+    return axiosUser.data.user;
+
     //console.log(axiosUser.data);
   }
   catch (error) {
@@ -92,7 +93,6 @@ const handleSignin = async (user) => {
 //   "Authorization": `bearer ${token}`,
 // },
 const signUp = () => {
-
   inquirer
     .prompt([
       {
@@ -136,7 +136,7 @@ const handleSignUp = async (user) => {
         'role': user.role,
       },
     };
-    axiosUser = await axios(config);
+    let axiosUser = await axios(config);
     //axiosUser = await axios(config);
     //console.log(axiosUser.data);
   }
@@ -160,6 +160,14 @@ const userMenu = () => {
     ])
     .then((answer) => {
       console.log(answer.menu);
+
+      if (answer.menu === 'Search') {
+        search();
+      }
+      else if (answer.menu === 'Request order') 
+      {
+        console.log('Request order');
+      }
     });
 };
 
@@ -170,11 +178,30 @@ const adminMenu = () => {
         name: 'menu',
         type: 'list',
         message: 'Choose a task',
-        choices: ['Search', 'Create', 'Read', 'Update', 'Delete'],
+        choices: ['Search part', 'Create part', 'Display all parts', 'Update part', 'Delete part'],
       },
     ])
     .then((answer) => {
       console.log(answer.menu);
+      switch (answer.menu) {
+        case 'Search part':
+          search();
+          break;
+        case 'Create part':
+          Create();
+          break;
+        case 'Display all parts':
+          Read();
+          break;
+        case 'Update part':
+          Update();
+          break;
+        case 'Delete part':
+          Delete();
+          break;
+        default:
+          break;
+      }
     });
 };
 
@@ -186,11 +213,279 @@ const clearLocalUser = () => {
   };
 };
 
-const search = () => { };
-const Create = () => { };
-const Read = () => { };
-const Update = () => { };
-const Delete = () => { };
+const search = () => {
+  inquirer
+    .prompt([
+      {
+        name: 'searchTerm',
+        type: 'input',
+        message: 'Search: ',
+      },
+    ])
+    .then((answer) => {
+      console.log('searchTerm', answer.searchTerm);
+      handleSearch(answer.searchTerm);
+    });
+};
+
+const handleSearch = async (searchTerm) => {
+  try {
+    let url = `${process.env.EXPRESS_SERVER}/api/v2/parts/${searchTerm}`;
+    let config = {
+      url,
+      method: 'get',
+      headers: {
+        'authorization': `bearer ${currentUser.token}`,
+      },
+    };
+    let axiosItem = await axios(config);
+    //axiosUser = await axios(config);
+    console.log(axiosItem.data);
+    return axiosItem.data;
+  }
+  catch (error) {
+    console.log(error);
+  }
+};
+
+const Create = () => {
+
+  let item = {
+    name: '',
+    quantity: '',
+    description: '',
+    manufacturer: '',
+    color: '',
+  };
+
+  inquirer
+    .prompt([
+      {
+        name: 'name',
+        type: 'input',
+        message: 'Enter item name ',
+      },
+      {
+        name: 'quantity',
+        type: 'number',
+        message: 'Enter item quantity ',
+      },
+      {
+        name: 'description',
+        type: 'input',
+        message: 'Enter item description ',
+      },
+      {
+        name: 'manufacturer',
+        type: 'input',
+        message: 'Enter item manufacturer ',
+      },
+      {
+        name: 'color',
+        type: 'input',
+        message: 'Enter item color ',
+      },
+    ])
+    .then((answer) => {
+      item.name = answer.name;
+      item.quantity = answer.quantity;
+      item.description = answer.description;
+      item.manufacturer = answer.manufacturer;
+      item.color = answer.color;
+      console.log('item', item);
+
+      handleCreate(item);
+    });
+};
+
+const handleCreate = async (item) => {
+  try {
+    let url = `${process.env.EXPRESS_SERVER}/api/v2/parts`;
+    let config = {
+      url,
+      method: 'post',
+      headers: {
+        'authorization': `bearer ${currentUser.token}`,
+      },
+      data: {
+        name: item.name,
+        quantity: item.quantity,
+        description: item.description,
+        manufacturer: item.manufacturer,
+        color: item.color,
+      },
+    };
+    let axiosUser = await axios(config);
+    //axiosUser = await axios(config);
+    //console.log('axiosUser.data: ', axiosUser.data);
+    console.log(`${axiosUser.data.name} part created`);
+  }
+  catch (error) {
+    console.log(error);
+  }
+};
+
+const Read = async () => {
+  try {
+    let url = `${process.env.EXPRESS_SERVER}/api/v2/parts`;
+    let config = {
+      url,
+      method: 'get',
+      headers: {
+        'authorization': `bearer ${currentUser.token}`,
+      },
+    };
+    let axiositem = await axios(config);
+    //axiosUser = await axios(config);
+    console.log('axiosUser.data: ', axiositem.data);
+    return axiositem.data;
+  }
+  catch (error) {
+    console.log(error);
+  }
+};
+
+
+const Update = async () => {
+  let itemsArr = await Read();
+  let item;
+  //console.log('itemsArr', itemsArr);
+  inquirer
+    .prompt([
+      {
+        name: 'userChoice',
+        type: 'list',
+        message: 'Choose an item to update.',
+        choices: itemsArr,
+      },
+    ])
+    .then(async (answer) => {
+      //console.log('item', answer.userChoice);
+      item = await handleSearch(answer.userChoice);
+      //console.log('item', item);
+      handleItemUpdate(item);
+    });
+};
+
+const handleItemUpdate = (item) => {
+  let updateItem = {
+    name: '',
+    quantity: '',
+    description: '',
+    manufacturer: '',
+    color: '',
+  };
+
+  inquirer
+    .prompt([
+      {
+        name: 'name',
+        type: 'input',
+        message: 'Enter name or press enter to skip',
+        default: `${item.name}`,
+      },
+      {
+        name: 'quantity',
+        type: 'number',
+        message: 'Enter quantity or press enter to skip',
+        default: item.quantity,
+      },
+      {
+        name: 'description',
+        type: 'input',
+        message: 'Enter description or press enter to skip',
+        default: item.description,
+      },
+      {
+        name: 'manufacturer',
+        type: 'input',
+        message: 'Enter manufacturer or press enter to skip',
+        default: item.manufacturer,
+      },
+      {
+        name: 'color',
+        type: 'input',
+        message: 'Enter updated item color ',
+        default: item.color,
+      },
+    ])
+    .then((answer) => {
+      updateItem.name = answer.name;
+      updateItem.quantity = answer.quantity;
+      updateItem.description = answer.description;
+      updateItem.manufacturer = answer.manufacturer;
+      updateItem.color = answer.color;
+      console.log('item', updateItem);
+      handleUpdate(item.id, updateItem);
+    });
+};
+
+const handleUpdate = async (id, item) => {
+  try {
+    let url = `${process.env.EXPRESS_SERVER}/api/v2/parts/${id}`;
+    let config = {
+      url,
+      method: 'put',
+      headers: {
+        'authorization': `bearer ${currentUser.token}`,
+      },
+      data: {
+        name: item.name,
+        quantity: item.quantity,
+        description: item.description,
+        manufacturer: item.manufacturer,
+        color: item.color,
+      },
+    };
+    let axiosUser = await axios(config);
+    //axiosUser = await axios(config);
+    //console.log('axiosUser.data: ', axiosUser.data);
+    console.log(`Part: ${axiosUser.data.name} updated`);
+  }
+  catch (error) {
+    console.log(error);
+  }
+};
+
+
+const Delete = async () => {
+  let itemsArr = await Read();
+  let item;
+  //console.log('Items Array', itemsArr);
+  inquirer
+    .prompt([
+      {
+        name: 'deletedItem',
+        type: 'list',
+        message: 'Which item do you want to delete ',
+        choices: itemsArr,
+      },
+    ])
+    .then(async (answer) => {
+      console.log('deletedItem', answer.deletedItem);
+      item = await handleSearch(answer.deletedItem);
+      handleDelete(item.id);
+    });
+};
+
+const handleDelete = async (id) => {
+  try {
+    let url = `${process.env.EXPRESS_SERVER}/api/v2/parts/${id}`;
+    let config = {
+      url,
+      method: 'delete',
+      headers: {
+        'authorization': `bearer ${currentUser.token}`,
+      },
+    };
+    let axiosUser = await axios(config);
+    //console.log(axiosUser.data);
+    console.log('Part deleted');
+  }
+  catch (error) {
+    console.log(error);
+  }
+};
 
 
 
